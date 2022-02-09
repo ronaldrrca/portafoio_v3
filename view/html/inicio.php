45e57aa0f754661b1186e2f546f26af8e76d6f7e
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2022-02-08 20:35:31 -0500

    Creando la estructura básica de carpetas y archivos

---
+++ view/html/inicio.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="desarrollo web, landing pages, paginas web, sitios web, 
+                                        aplicaciones web, diseño responsivo, web development, 
+                                        web pages, web sites, web apps, responsive design.">
+    <meta name="robots" content="index.follow">
+    <link rel="shortcut icon" href="#" /><!--Fixing favicon-->
+    <title>Ronald Rodríguez Campillo</title>
+</head>
+<body>
+    <header>
+            <?php include 'header.php'?>
+    </header>
+    <main>
+            
+    </main>
+    <footer>
+
+    </footer>
+    <script src="../js/header.js"></script>
+</body>
+</html>
