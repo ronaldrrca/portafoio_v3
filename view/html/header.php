3fd1d0c5bba641879e073a7487bef3ce8969bdb3
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2022-02-08 20:46:29 -0500

    Agregando los estilos generales

---
+++ view/html/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/generals.css">
+    <!-- <link rel="stylesheet" href="../css/768p.css" media="screen and (min-width: 768px)"> -->
+    <!-- <link rel="stylesheet" href="../css/1024p.css" media="screen and (min-width: 1024px)"> -->
+    <!-- <link rel="stylesheet" href="style.css" media="screen and (min-width: 1300px)"> -->
+    <!-- <link rel="stylesheet" href="../css/1500p.css" media="screen and (min-width: 1500px)"> -->
+</head>
+<body>
+    
+</body>
+</html>
