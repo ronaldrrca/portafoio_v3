75186200d67468909e77456c518e5cff6835545b
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2022-02-09 10:54:31 -0500

    Agregando la sección introducingMyself en el inicio

--- view/html/header.php
+++ view/html/header.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/generals.css">
-    <!-- <link rel="stylesheet" href="../css/768p.css" media="screen and (min-width: 768px)"> -->
+    <link rel="stylesheet" href="../css/768p.css" media="screen and (min-width: 768px)">
     <link rel="stylesheet" href="../css/1024p.css" media="screen and (min-width: 1024px)">
-    <!-- <link rel="stylesheet" href="style.css" media="screen and (min-width: 1300px)"> -->
+    <!-- <link rel="stylesheet" href="1300p.css" media="screen and (min-width: 1300px)"> -->
     <!-- <link rel="stylesheet" href="../css/1500p.css" media="screen and (min-width: 1500px)"> -->
 </head>
 <body>
