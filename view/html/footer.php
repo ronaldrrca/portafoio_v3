30dad7e4a2697b6613edfbe5d3d8c2910765f119
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2022-03-12 14:54:19 -0500

    Cambiando los íconos del footer por unos con tamaño fijo para mejorar la performance

--- view/html/footer.php
+++ view/html/footer.php
@@ -7,11 +7,11 @@
 </head>
 <body>
     <div id="footer_socialMediaIcons">
-        <a href="https://www.linkedin.com/in/ronald-rodríguez-campillo-66b12771" target="_blank"><img src="../assets/icons/linkedin_icon.svg" alt="icono de linkedin"></a>
-        <a href="https://github.com/ronaldrrca" target="_blank"><img src="../assets/icons/github_icon.svg" alt="icono de github"></a>
-        <a href="https://www.instagram.com/ronald_rodriguez_campillo" target="_blank"><img src="../assets/icons/instagram_icon.svg" alt="icono de instagram"></a>
-        <a href="https://twitter.com/ronaldrrca?s=03" target="_blank"><img src="../assets/icons/twitter_icon.svg" alt="icono de twitter"></a>
-        <a href="https://ronaldweb.com" target="_blank"><img src="../assets/icons/facebook_icon.svg" alt="icono de facebook"></a>
+        <a href="https://www.linkedin.com/in/ronald-rodríguez-campillo-66b12771" target="_blank"><img src="../assets/icons/linkedin_icon_40p.svg" alt="icono de linkedin"></a>
+        <a href="https://github.com/ronaldrrca" target="_blank"><img src="../assets/icons/github_icon_40p.svg" alt="icono de github"></a>
+        <a href="https://www.instagram.com/ronald_rodriguez_campillo" target="_blank"><img src="../assets/icons/instagram_icon_40p.svg" alt="icono de instagram"></a>
+        <a href="https://twitter.com/ronaldrrca?s=03" target="_blank"><img src="../assets/icons/twitter_icon_40p.svg" alt="icono de twitter"></a>
+        <a href="https://ronaldweb.com" target="_blank"><img src="../assets/icons/facebook_icon_40p.svg" alt="icono de facebook"></a>
     </div>
     <p>Proyecto desarrollado por RONALD RODRÍGUEZ CAMPILLO</p>
     <p>Tecnólogo en Análisis y Desarrollo de Sistemas de Información</p>
